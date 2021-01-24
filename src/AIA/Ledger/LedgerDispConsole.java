41fa322b6c173ebd718b48d72104b311c925f557
Jonghyun <jonghyun.yoo0323@gmail.com>
2021-01-24 19:58:12 +0900

    LedgerDispConsole에서 switch문으로 구현했던 선택지를 HashMap을 이용해 함수를 불러오는것으로 변경.

--- src/AIA/Ledger/LedgerDispConsole.java
+++ src/AIA/Ledger/LedgerDispConsole.java
@@ -1,8 +1,10 @@
 package AIA.Ledger;
 
-import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 
 /**
@@ -11,55 +13,65 @@ import java.util.Scanner;
  *
  */
 public class LedgerDispConsole {
-	public Scanner sc = new Scanner(System.in);
-	public LedgerWriter saver = new LedgerWriter();
+	public static Scanner sc = new Scanner(System.in);
+	public static LedgerWriter saver = new LedgerWriter();
 	
-	public void work(Record record) throws IOException {
-		 String todo = "";
+	public static void work(Record record) throws Exception {
+		HashMap<String, Method> order = new HashMap<String, Method>();
+		order.put("1", LedgerDispConsole.class.getMethod("add", Record.class));
+		order.put("2", LedgerDispConsole.class.getMethod("delete", Record.class));
+		order.put("3", LedgerDispConsole.class.getMethod("edit", Record.class));
+		order.put("4", LedgerDispConsole.class.getMethod("search", Record.class));
+		order.put("5", LedgerDispConsole.class.getMethod("print", Record.class));
+		Set<String> keys = order.keySet();
+		String todo = "";
         while(!todo.equals("6")){
             System.out.println("=============Select One===============");
             System.out.println("1. Add an invoice \n2. Delete an invoice \n3. Edit an invoice \n"
             		+ "4. Search for an invoice \n5: Print all records "+
             		"\n6: Exit");
             System.out.println("======================================");
+            
             todo = sc.nextLine();
-            switch (todo){
-                case "1":
-                	add(record);
-                    break;
-                case "2":
-                    System.out.println("Enter the number of invoice you want to delete");
-                    String numtodel = sc.nextLine();
-                    int dnum = Integer.parseInt(numtodel);
-                    record.deleteRecord(dnum);
-                    break;
-                case "3":
-                    System.out.println("Enter the number of invoice you want to edit");
-                    String numtoedit = sc.nextLine();
-                    int nume = Integer.parseInt(numtoedit);
-                    record.editRecord(nume);
-                    break;
-                case "4":
-                    System.out.println("Enter what you know about it");
-                    String clue = sc.nextLine();
-                    record.search(clue);
-                    break;
-                case "5":
-                    record.printAllRecords();
-                    break;
-                case "6":
-                	break;
-                default:
-                    System.out.println("\nAN ERROR MESSAGE: Please enter runnable commands\n");
-            }
+            if(keys.contains(todo)) {
+            	
+            	order.get(todo).invoke(null, record);
+            
+            }else if(!todo.equals("6")) {
+            	System.out.println("\nAN ERROR MESSAGE: Please enter runnable commands\n");
+            }    
         }
         saver.saveRecords(record);
         System.out.println("All changes saved");
-		
         sc.close();
     }
 
-	private void add(Record record) {
+	public static void print(Record record) {
+		record.printAllRecords();
+		
+	}
+
+	public static void search(Record record) {
+		System.out.println("Enter what you know about it");
+        String clue = sc.nextLine();
+        record.search(clue);
+	}
+
+	public static void edit(Record record) {
+		System.out.println("Enter the number of invoice you want to edit");
+        String numtoedit = sc.nextLine();
+        int nume = Integer.parseInt(numtoedit);
+        record.editRecord(nume);
+	}
+
+	public static void delete(Record record) {
+		System.out.println("Enter the number of invoice you want to delete");
+        String numtodel = sc.nextLine();
+        int dnum = Integer.parseInt(numtodel);
+        record.deleteRecord(dnum);
+	}
+
+	public static void add(Record record) {
 		ArrayList<String> invoice = new ArrayList<String>();
 		System.out.println("earn(R)? or pay(C)?");
         invoice.add(sc.nextLine());
@@ -82,12 +94,11 @@ public class LedgerDispConsole {
             System.out.println("Did you get receipt 1.Yes or 2.No?");
             String receipt = sc.nextLine();
             if (receipt.equals("1")){
-                invoice.add("Yes");
+                invoice.add("Have");
             }
             if(receipt.equals("2")){
-                invoice.add("No");
+                invoice.add("Do not Have");
             }
-            
         }
         record.addRecord(invoice);
 	}
--- src/AIA/Ledger/LedgerMain.java
+++ src/AIA/Ledger/LedgerMain.java
@@ -1,6 +1,5 @@
 package AIA.Ledger;
 
-import java.io.IOException;
 /**
  * Ledger를 기동하는 드라이버 클래스. 친절한 인사말로 시작한다.
  * @author Jonghyun John Yoo PC
@@ -8,15 +7,14 @@ import java.io.IOException;
  */
 public class LedgerMain {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		System.out.println("JH가계부에 오신걸 환영합니다! \n");
 		LedgerReader starter = new LedgerReader();
 		Record records = new Record();
 		
 		starter.readRecords(records);
 		
-		LedgerDispConsole disp = new LedgerDispConsole();
-		disp.work(records);
+		LedgerDispConsole.work(records);
 		System.out.println("이용해 주셔서 감사합니다. 좋은 하루 되세요!");
 	}
 }
